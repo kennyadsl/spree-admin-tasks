c92a938ff53180b82c712cbd6c45ab7df96b820d
Torsten Rüger <torsten@lightning.nu>
2011-09-09 20:55:04 +0300

    fixed naming added report

--- lib/spree_admin_tasks.rb
+++ lib/spree_admin_tasks.rb
@@ -6,9 +6,11 @@ module SpreeAdminTasks
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/models/AdminTask.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+      Rails.env.production? ? require('reports_controller_decorator') : load('reports_controller_decorator.rb')
+      Admin::ReportsController::AVAILABLE_REPORTS.merge!({ :open_orders => {:name => "Maksamatta", :description => "Maksamattomat tilaukset"}  })
     end
 
     config.to_prepare &method(:activate).to_proc
